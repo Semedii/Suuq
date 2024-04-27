d30037a4b9fb0061a7bf88015692b7899ce2febe
semedii <abdisamed2023@hotmail.com>
2024-04-27 20:10:39 +0300

    fixes

--- lib/providers/signup/signup_notifier.dart
+++ lib/providers/signup/signup_notifier.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:suuq/providers/signup/signup_state.dart';
