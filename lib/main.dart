9bea106e06dd3acf2c6f63c11eb6ea1b338d84d1
semedii <abdisamed2023@hotmail.com>
2024-04-27 18:39:02 +0300

    added firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:suuq/firebase_options.dart';
 import 'package:suuq/router/app_router.dart';
 import 'package:suuq/utils/app_theme.dart';
 
-void main() {
-  runApp( ProviderScope(child: MyApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-   MyApp({super.key});
+  MyApp({super.key});
 
   final _appRouter = AppRouter();
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp.router(
+    return MaterialApp.router(
       theme: AppTheme.appThemeData,
       routerConfig: _appRouter.config(),
     );
   }
 }
-
