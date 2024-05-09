209a6a14894b9911a4a5fc653e454d844ee0be22
semedii <abdisamed2023@hotmail.com>
2024-05-09 20:50:02 +0300

    added confirmation step

--- lib/pages/checkout_page/confirmation_step.dart
+++ lib/pages/checkout_page/confirmation_step.dart
@@ -5,6 +5,45 @@ class ConfirmationStep extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(width: 300, height: 300, color: Colors.red,);
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _buildIcon(),
+          const SizedBox(height: 24),
+          _buildTitle(),
+          const SizedBox(height: 16),
+          _buildDescriptionText(),
+          TextButton(onPressed: (){}, child: Text("Call us instead?"))
+        ],
+      ),
+    );
+  }
+
+  Icon _buildIcon() {
+    return const Icon(
+      Icons.check_circle_outline,
+      color: Colors.green,
+      size: 120,
+    );
+  }
+
+  Text _buildTitle() {
+    return const Text(
+      "Thank you for shopping with us!",
+      style: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Text _buildDescriptionText() {
+    return const Text(
+      "Your order has been received. We will confirm the payment and contact you within 5 minutes.",
+      style: TextStyle(fontSize: 16),
+      textAlign: TextAlign.center,
+    );
   }
 }
