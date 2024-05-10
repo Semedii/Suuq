5a1244ba2a42dd160ef781e939aa947c9e304a16
semedii <abdisamed2023@hotmail.com>
2024-05-10 16:23:55 +0300

    fetches orders by email

--- lib/services/order_data_service.dart
+++ lib/services/order_data_service.dart
@@ -1,14 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:suuq/models/order.dart';
 
-class OrderDataService{
-final FirebaseFirestore db = FirebaseFirestore.instance;
-    Future<void> addNewOrder(OrderModel order) async {
+class OrderDataService {
+  final FirebaseFirestore db = FirebaseFirestore.instance;
+  Future<void> addNewOrder(OrderModel order) async {
     try {
       final docRef = db
           .collection("orders")
           .withConverter(
-             fromFirestore:OrderModel.fromFirestore,
+            fromFirestore: OrderModel.fromFirestore,
             toFirestore: (OrderModel order, options) => order.toFirestore(),
           )
           .doc();
@@ -17,17 +17,17 @@ final FirebaseFirestore db = FirebaseFirestore.instance;
       print("Error fetching products: $e");
     }
   }
-    Future<List<OrderModel?>> fetchUsersOrders(String email) async {
+
+  Future<List<OrderModel?>> fetchUsersOrders(String email) async {
     try {
-      final collectionRef = db
-          .collection("orders")
-          .withConverter(
+      final collectionRef = db.collection("orders").withConverter(
             fromFirestore: OrderModel.fromFirestore,
             toFirestore: (order, _) => order.toFirestore(),
           );
-
-      final querySnapshot = await collectionRef.get();
-      List<OrderModel> orders = querySnapshot.docs.map((doc) => doc.data()).toList();
+      final querySnapshot =
+          await collectionRef.where("customer.email", isEqualTo: email).get();
+      List<OrderModel> orders =
+          querySnapshot.docs.map((doc) => doc.data()).toList();
       return orders;
     } catch (e) {
       print("Error fetching products: $e");
