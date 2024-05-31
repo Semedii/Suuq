23076dcf25f44662366e60dc6be252fbbcecc403
semedii <abdisamed2023@hotmail.com>
2024-05-31 18:27:20 +0300

    used CustomScrollView

--- lib/pages/homepage/show_all_page.dart
+++ lib/pages/homepage/show_all_page.dart
@@ -1,6 +1,5 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:suuq/components/product_card.dart';
 import 'package:suuq/models/product.dart';
@@ -10,7 +9,7 @@ import 'package:suuq/notifiers/category/category_state.dart';
 
 @RoutePage()
 class CategoryPage extends ConsumerWidget {
-  const CategoryPage({required this.categoryName,super.key});
+  const CategoryPage({required this.categoryName, super.key});
   final String categoryName;
 
   @override
@@ -18,42 +17,47 @@ class CategoryPage extends ConsumerWidget {
     final state = ref.watch(categoryNotifierProvider);
     AppLocalizations localizations = AppLocalizations.of(context)!;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("${localizations.all} $categoryName"),
-      ),
-      body:
-      
-      _mapStateToWidget(state, ref)
-    );
-  }
-  Widget _mapStateToWidget(CategoryState state, WidgetRef ref){
-    if(state is CategoryInitialState){
-       WidgetsBinding.instance.addPostFrameCallback((_) {
-      ref.read(categoryNotifierProvider.notifier).initPage(categoryName);
-       });
-    } else if(state is CategoryLoadedState){
-     return _buildPageBody(state.products);
-    }
-    return const Center(child: CircularProgressIndicator(),);
+        appBar: AppBar(
+          title: Text("${localizations.all} $categoryName"),
+        ),
+        body: _mapStateToWidget(context, state, ref));
   }
 
-  Padding _buildPageBody(List<Product?> products) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 8.0,
-          mainAxisSpacing: 8.0,
-          childAspectRatio: 0.7, // Adjust this ratio based on your card's design
-        ),
-        itemCount: products.length,
-        itemBuilder: (context, index) {
-          final product = products[index];
-          if (product != null) return ProductCard(product: product);
-          return const SizedBox.shrink(); // Return an empty widget if product is null
-        },
-      ),
+  Widget _mapStateToWidget(
+      BuildContext context, CategoryState state, WidgetRef ref) {
+    if (state is CategoryInitialState) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        ref.read(categoryNotifierProvider.notifier).initPage(categoryName);
+      });
+    } else if (state is CategoryLoadedState) {
+      return _buildPageBody(context, state.products);
+    }
+    return const Center(
+      child: CircularProgressIndicator(),
     );
+  }
+
+  Padding _buildPageBody(BuildContext context, List<Product?> products) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: CustomScrollView(
+          slivers: [
+            SliverGrid(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 8.0,
+                mainAxisSpacing: 8.0,
+                childAspectRatio: 0.7,
+              ),
+              delegate: SliverChildBuilderDelegate((context, index) {
+                final product = products[index];
+                if (product != null) {
+                  return ProductCard(product: product);
+                }
+                return const Text("no items found");
+              }, childCount: products.length),
+            )
+          ],
+        ));
   }
 }
